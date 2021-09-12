eca6cf4f168f07452402d21350a644356f6238e6
phu024 <phuwadon.dev@gmail.com>
2021-09-12 21:01:08 +0700

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+
+import Navbar from "./components/Navbar";
+
+import Users from "./components/Users";
+
+import UserCreate from "./components/UserCreate";
+
+
+export default function App() {
+
+ return (
+
+   <Router>
+
+     <div>
+
+       <Navbar />
+
+       <Switch>
+
+         <Route exact path="/" component={Users} />
+
+         <Route exact path="/create" component={UserCreate} />
+
+       </Switch>
+
+     </div>
+
+   </Router>
+
+ );
+
 }
-
-export default App;
